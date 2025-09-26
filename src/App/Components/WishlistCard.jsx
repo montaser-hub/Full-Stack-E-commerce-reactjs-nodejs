2c7f2ca238bd7c1703c83946d64f98ce18e29ba6
tarekhamdy99 <tarekhamdy7469@gmail.com>
2025-09-26 23:13:06 +0300

    First Model

---
+++ src/App/Components/WishlistCard.jsx
@@ -0,0 +1,33 @@
+function WishlistCard(props) {
+  return (
+    <div className="relative w-80 h-[393px] bg-white rounded-lg border border-gray-200 shadow-md overflow-hidden">
+      <div className="relative w-full h-48">
+        <img src={props.image} alt={props.title} className="w-full h-full object-cover rounded-t-lg" />
+          <div className= " absolute top-2 left-2 flex items-center justify-center h-7 px-2 rounded-full font-semibold text-xs text-white bg-red-500">
+            <span>{props.discount}</span>
+          </div>
+      </div>
+      <div className="p-4 flex flex-col justify-between">
+        <div>
+          <h3 className="text-neutral-900 text-lg font-semibold">{props.title}</h3>
+          <div className="flex items-center gap-2 mt-1">
+            <span className="text-blue-600 text-xl font-bold">{props.price}</span>
+            <span className="text-gray-400 text-sm">{props.oldprice}</span>
+          </div>
+            <div className="mt-4">
+            <span className= "mt-4 text-sm px-2 py-1 rounded-full font-semibold bg-red-500 text-white"> {props.stock} </span>
+            </div>
+        </div>
+      </div>
+      <div className="absolute bottom-4 left-4 right-4 flex gap-2">
+        <button className="flex-1 h-10 flex items-center justify-center font-medium text-white bg-blue-600 rounded-md hover:bg-blue-700 disabled:opacity-40">
+          Move to Cart
+        </button>
+        <button className="flex-1 h-10 flex items-center justify-center font-medium text-neutral-900 bg-white border border-gray-200 rounded-md hover:bg-gray-100 disabled:opacity-40">
+          Remove
+        </button>
+      </div>
+    </div>
+  );
+};
+export default WishlistCard;
